88dd47666cfab18c63c81392b03145b4501341df
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2021-11-17 22:15:03 -0300

    Início do layout da criação de quizz.

---
+++ scripts/quizz_creation.js
@@ -0,0 +1,10 @@
+// Validate number of chars of title
+function title_validation(title) {
+    const num_chars = title.length;
+    return num_chars >= 20 && num_chars <= 65;
+}
+
+// Validate image URL
+function URL_validation(URL) {
+    // TODO
+}
