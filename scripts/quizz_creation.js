174b462aa025f43f010c18b5a254d09d193fba62
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2021-11-18 16:46:25 -0300

    Validação de informações básicas de quizz.

--- scripts/quizz_creation.js
+++ scripts/quizz_creation.js
@@ -6,5 +6,33 @@ function title_validation(title) {
 
 // Validate image URL
 function URL_validation(URL) {
-    // TODO
+    const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+
+    return pattern.test(URL);
+}
+
+function validate_basic_info() {
+    const info_entry = document.querySelector(".info-entry");
+    const title = info_entry.querySelector(".title").value;
+    const image_URL = info_entry.querySelector(".image-URL").value;
+    const num_questions = parseInt(info_entry.querySelector(".num-questions").value);
+    const num_levels = parseInt(info_entry.querySelector(".num-levels").value);
+
+    // Alert user if some input is invalid
+    const info_is_valid = 
+        title_validation(title) &&
+        URL_validation(image_URL) &&
+        num_questions >= 3 &&
+        num_levels >= 2;
+
+    if (info_is_valid) {
+        alert("Tudo certo pra prosseguir.");
+    } else {
+        alert("Insira os dados corretamente");
+    }
 }
